3e7dee5d3e8f2422aaf4d53a6dd1f5df0c1e5fe0
djalmabarbosa <djalma.barboa@ufr.edu.br>
2024-12-04 16:50:11 -0400

    Final da aula.

--- teste.R
+++ teste.R
@@ -1,1 +1,24 @@
 # Testando a sincronia com o github
+
+# Instalando pacotes
+install.packages("tidyverse")
+
+# Carregando o pacote
+library(tidyverse)
+
+# Verificando os pacotes instalados
+sessionInfo()
+
+# Exemplo de função no R
+x <- c(1,1,2,3,5,8)
+
+
+y <- c(2,NA)
+mean(y,1)
+
+
+
+# Calcula a média de x
+mean(x)
+
+
